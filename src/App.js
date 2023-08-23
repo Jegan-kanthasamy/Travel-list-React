923adadc9d507b53b0ca26d45b366996809bf8d0
Jegan-kanthasamy <jegankanthasamy@outlook.com>
2023-08-23 18:07:28 +0530

    Add static functions to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import { useState } from "react";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🎒</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="packing-list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X item in your list, and you already packed X</em>
+    </footer>
+  );
 }
