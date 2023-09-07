da06f4c5e56b54de642332c079e7d20cbb5264ee
Jegan-kanthasamy <jegankanthasamy@outlook.com>
2023-09-07 15:26:51 +0530

    Create seperate folder and files for every components

---
+++ src/components/Item.js
@@ -0,0 +1,19 @@
+// item function (it  is used to be as child comp. of PackingList function to create elements)
+
+export default function Item({ item, onDeleteItem, onToggle }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggle(item.id);
+        }}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : null}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
