e580a177732cdf59d9bcc0c4c07c6f7f74244e4e
Jegan-kanthasamy <jegankanthasamy@outlook.com>
2023-09-07 12:40:51 +0530

    add sort and clear list functionality to the page

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ export default function App() {
       )
     );
   }
+
+  function handleClearList() {
+    setAddItem([]);
+  }
   return (
     <div className="app">
       <Logo />
@@ -32,6 +36,7 @@ export default function App() {
         addItem={addItem}
         onDeleteItem={deleteItem}
         onToggle={handleToggleOnClick}
+        onClearList={handleClearList}
       />
       <Stats item={addItem} />
     </div>
@@ -109,11 +114,26 @@ function Form({ newItem }) {
 
 // PackingList (it shows the items quantity , checkbox and remove item icon )
 
-function PackingList({ addItem, onDeleteItem, onToggle }) {
+function PackingList({ addItem, onDeleteItem, onToggle, onClearList }) {
+  const [sortItem, setSortItem] = useState("item");
+
+  let sortedItems;
+
+  if (sortItem === "item") sortedItems = addItem;
+  else if (sortItem === "description") {
+    sortedItems = addItem
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  } else {
+    sortedItems = addItem
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {addItem.map(items => (
+        {sortedItems.map(items => (
           <Item
             item={items}
             onDeleteItem={onDeleteItem}
@@ -122,6 +142,14 @@ function PackingList({ addItem, onDeleteItem, onToggle }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortItem} onChange={e => setSortItem(e.target.value)}>
+          <option value="item">Sort by Item</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed</option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
     </div>
   );
 }
